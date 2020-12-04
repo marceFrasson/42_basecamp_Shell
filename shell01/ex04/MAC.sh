f469c09815177f27884155a7f942de7088b87d96
mfrasson <mfrasson@student.42sp.org.br>
2020-12-04 10:13:03 +0000

    shel01

---
+++ shell01/ex04/MAC.sh
@@ -0,0 +1,1 @@
+ifconfig | grep ethe | sed 's/\(.*\).../\1/'
---
+++ shell01/ex07/r_dwssap.sh
@@ -0,0 +1,1 @@
+awk -F":" 'NR % 2 == 0 {print $1}' /etc/passwd | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' "," |  sed 's/,$/./g; s/,/, /g'
